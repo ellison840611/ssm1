cbaa4cf0d890731b9c9b8f1843cf074ed20cf44c
ellison <wwang840611@126.com>
2018-12-30 23:34:51 +0800

    测试

--- src/main/java/com/elin4it/ssm/controller/UserController.java
+++ src/main/java/com/elin4it/ssm/controller/UserController.java
@@ -36,6 +36,7 @@ public class UserController {
         //设置响应的jsp视图
         modelAndView.setViewName("findUser");
 
+        System.out.println("github测试");
         return modelAndView;
     }
 }
